48ec490f52768b7e88877cfd3fd8112afaf44324
Deniz00000 <92266788+Deniz00000@users.noreply.github.com>
2021-11-04 11:23:52 +0300

    Add files via upload

---
+++ try catch1/try catch1/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace try_catch1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int sayi = Convert.ToInt32(textBox1.Text);
+            int sayi2 = Convert.ToInt32(textBox2.Text);
+            try
+            {
+                int sayi3 = Convert.ToInt32(textBox3.Text);
+                int sayi4 = Convert.ToInt32(textBox4.Text);
+                int toplam = sayi + sayi2 + sayi3 + sayi4;
+                label1.Text = toplam.ToString();
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show("Hatalı" + ex.Message);
+            }
+        }
+    }
+}
